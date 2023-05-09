741be52c842229f362d5cc9d0125484efaf7b9aa
Tang Hien Phan <phan05042000@gmail.com>
2023-05-09 14:11:44 +0700

    Add base model for project

--- app/Models/Template.php
+++ app/Models/Template.php
@@ -4,9 +4,8 @@ namespace App\Models;
 
 use App\Traits\UUID;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
-class Template extends Model
+class Template extends BaseModel
 {
     use HasFactory, UUID;
 
